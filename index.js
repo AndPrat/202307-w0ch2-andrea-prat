d1fe98955964b6721d45e85f9edeca8f45420e06
Andrea Prat <andr8ide@gmail.com>
2023-07-06 21:26:08 +0200

    Change naming functions

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
 const generateDeckCards = () => {
-  const suitsCards = [
-    "♠️ (picas)",
-    "♣️ (tréboles)",
-    "♦️ (diamantes)",
-    "♥️ (corazones)",
-  ];
+  const suitsCards = ["♠️", "♣️", "♦️", "♥️"];
 
-  const numbersCards = [
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10,
-    "J (jota)",
-    "Q (reina)",
-    "K (rey)",
-    "A (as)",
-  ];
+  const numbersCards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 
   const deck = [];
 
